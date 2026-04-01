ca8baffd4638628134409a2874f761e2737e8d85
jhpark676 <woguddlrj676@kaist.ac.kr>
2026-04-01 07:12:17 +0000

    remove: frontend module (need to fix)

--- trinity/stages/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Internal stage wrappers used by the Trinity package facade."""
-
-from .backend import BackendStage
-from .frontend import FrontendStage
-from .optimizer import OptimizerStage
-
-__all__ = ["BackendStage", "FrontendStage", "OptimizerStage"]
